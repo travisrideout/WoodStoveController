65ee534a6c74edbc53eb82c39c362347c0b9bca3
Travi <Travi@MSI>
2016-11-05 20:20:15 -0400

    Add project files.

---
+++ WoodStoveController/WoodStoveController.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		WoodStoveController.ino
+ Created:	11/5/2016 8:20:11 PM
+ Author:	Travi
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
