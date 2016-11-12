7a9761c5ff0018a703e684cff46be9dae72e0c4e
Travis Rideout <Travis.Rideout518@gmail.com>
2016-11-12 18:58:21 -0500

    Added RemoteUI Project

---
+++ WSC_RemoteUI/WSC_RemoteUI.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		WSC_RemoteUI.ino
+ Created:	11/12/2016 6:55:52 PM
+ Author:	Travi
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
--- WoodStoveController/WoodStoveController.ino
+++ WoodStoveController/WoodStoveController.ino
@@ -1,15 +1,52 @@
 /*
  Name:		WoodStoveController.ino
  Created:	11/5/2016 8:20:11 PM
- Author:	Travi
+ Author:	Travis Rideout
 */
 
-// the setup function runs once when you press reset or power the board
-void setup() {
+#include "WoodStoveController.h"
 
+void setup() {
+	Serial.begin(115200);
+
+	InitializeStateVariables();
 }
 
-// the loop function runs over and over again until power down or reset
 void loop() {
-  
+	//if change in set temp then change state
+	//get room temps
+	//if temp below setpoint - temp_slack then heat
+	//if temp above setpoint then idle
+	//if any alarms then off state and send alarm notification
+	//if trying to heat and firebox temp not going up then out of fuel
+	
+}
+
+void InitializeStateVariables() {
+	off = {
+		LOW,	//fan 1
+		LOW,	//fan2
+		0,		//stokeFanSpeed_cmd
+		0,		//damperValvePosition_cmd
+		30,		//fireBoxTemp_cmd
+		30,		//heatExchangerTemp_cmd
+	};
+
+	idle = {
+		LOW,	//fan 1
+		LOW,	//fan2
+		0,		//stokeFanSpeed_cmd
+		10,		//damperValvePosition_cmd
+		300,	//fireBoxTemp_cmd
+		100,	//heatExchangerTemp_cmd
+	};	
+
+	heat = {
+		HIGH,	//fan 1
+		HIGH,	//fan2
+		100,	//stokeFanSpeed_cmd
+		100,	//damperValvePosition_cmd
+		30,		//fireBoxTemp_cmd
+		30,		//heatExchangerTemp_cmd
+	};
 }
